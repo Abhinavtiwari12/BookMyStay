69b245fabdefcf90f7c0858f26cac57139ab50be
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-03-08 22:52:12 +0530

    create owner controller and add create owner, login owner, logout owner, and owner profile

---
+++ src/controller/owner.controller.js
@@ -0,0 +1,134 @@
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { Owner } from "../models/owner.model.js";
+import { findUser, registerUser } from "../service/user.service.js";
+
+
+
+
+const generateAccessAndRefereshTokens = async(userId) => {
+   try {
+     const owner = await Owner.findById(userId)
+       if (!owner) {
+    throw new Error("User not found while generating tokens");
+  }
+     const accessToken = owner.generateAccessToken()
+     const refreshToken = owner.generateRefreshToken()
+ 
+     owner.refreshToken = refreshToken
+     await owner.save({ validateBeforeSave: false })
+ 
+     return {accessToken, refreshToken}
+   } catch (error) {
+        throw new ApiError(500, "Somthing went wrong while generating access and refresh token!!")
+   }
+}
+
+
+
+const createUser = asyncHandler(async (req, res) => {
+    const  { hotelName, password, email, ownerName, phoneNumber, address, availableRooms } =req.body
+
+    if (
+        [hotelName, password, email, phoneNumber, address, ownerName, availableRooms ].some((feild) => feild?.trim() === "")
+    ) {
+        throw new ApiError(400, "All feilds are require")
+    }
+
+    const checkExistedUser = {
+        email,
+        email
+    }
+    const existedUser = await findUser(checkExistedUser)
+
+    const createUser = {
+        hotelName,
+        email,
+        password,
+        phoneNumber,
+        ownerName,
+        address,
+        availableRooms
+    }
+
+    const registeredUser = await registerUser(createUser)
+
+    return res.status(201).json(
+        new apiResponse(201, registeredUser.data, registeredUser.message)
+    )
+})
+
+
+
+const userlogin = asyncHandler(async (req, res) => {
+
+    const {email, userName, password} = req.body
+
+    if (!userName && !email) {
+        throw new ApiError(401, "Username or email is require.")
+    }
+
+    const query = email ? { email } : { userName };
+
+    const user = await User.findOne(query)
+
+    if (!user) {
+        throw new ApiError(400, "username, email or password is wrong.")
+    }
+
+    const checkPassword = await user.isPasswordCorrect(password)
+
+    if (!checkPassword) {
+        throw new ApiError(400, "username, email or password is wrong.")
+    }
+
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id).select("-password")
+
+    const options = {
+        httpOnly: true,
+        secure: false
+    }
+
+    return res.status(200).cookie("accessToken", accessToken, options).cookie("refreshToken", refreshToken, options)
+    .json(new apiResponse(200, { user: loggedInUser, accessToken, refreshToken}, "user loggedIn successfull."))
+})
+
+
+const userlogout = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset: {
+                refreshToken: 1
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new apiResponse(200,{}, "User logout success."))
+
+})
+
+
+const userProfile = async (req, res) => {
+  return res.status(200).json({
+    success: true,
+    user: req.user,
+  });
+};
+
+
+export { createUser, userlogin, userlogout, userProfile }
