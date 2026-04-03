392f11986c6eead460e774a0f46c4c108bfc8eb8
Abhinav Tiwari <tiwariabhinav2244@gmail.com>
2026-04-03 22:58:49 +0530

    add updateAvailableRooms route to owner API

--- src/router/owner.route.js
+++ src/router/owner.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { checkIn, checkOut, createOwner, ownerlogin, ownerlogout, ownerProfile, seeBookings, seeUserDetails } from "../controller/owner.controller";
+import { checkIn, checkOut, createOwner, ownerlogin, ownerlogout, ownerProfile, seeBookings, seeUserDetails, updateAvailableRooms } from "../controller/owner.controller";
 import { verifyOwnerjwt } from "../middleware/autho.middleware";
 
 
@@ -12,10 +12,9 @@ router.route('/createOwner').post(createOwner)
 router.route('/ownerlogin').post(ownerlogin)
 router.route('/ownerlogout').post(verifyOwnerjwt,ownerlogout)
 router.route('/ownerProfile/:bookingId').post(verifyOwnerjwt,ownerProfile)
-router.route('/checkIn/:bookingId').post(verifyOwnerjwt,checkIn)
-router.route('/checkOut/:bookingId').post(verifyOwnerjwt,checkOut)
 router.route('/seeBookings').post(verifyOwnerjwt,seeBookings)
 router.route('/seeUserDetails/:bookingId').post(verifyOwnerjwt,seeUserDetails)
+router.route('/updateAvailableRooms').post(verifyOwnerjwt, updateAvailableRooms)
 
 
 
